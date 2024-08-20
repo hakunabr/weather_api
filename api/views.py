5b3b416f0ba10f508210a541b873d410c6dafbed
Gabriel Figueira <gabriel.s.figueira@gmail.com>
2024-08-20 18:10:09 -0300

    fixed some typos, change some erros on last commit

--- api/views.py
+++ api/views.py
@@ -18,10 +18,12 @@ def get_city_coords(city_name):
     api_key = settings.API_KEY
     city_info_url = 'http://api.openweathermap.org/geo/1.0/direct?q={}&limit=1&appid={}'.format(city_name, api_key)
     coords_response = requests.get(city_info_url)
+    print(coords_response.json())
     city_coods = (coords_response.json()[0]['lat'], coords_response.json()[0]['lon'])
     return city_coods
 
 def get_weather_info(coords):
-    weather_url = 'https://api.openweathermap.org/data/3.0/onecall?lat={}&lon={}&appid={}'.format(city_coods[0], city_coods[1], api_key)
+    api_key = settings.API_KEY
+    weather_url = 'https://api.openweathermap.org/data/3.0/onecall?lat={}&lon={}&appid={}'.format(coords[0], coords[1], api_key)
     weather_response = requests.get(weather_url)
     return Response(weather_response.json())
